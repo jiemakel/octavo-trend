6f7d974dc3d752e1b2740a14924c2f02388789af
Eetu Mäkelä <eetu.makela@iki.fi>
2017-05-15 11:33:10 +0300

    update webpack configurations for publishing

--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,10 @@
-var glob = require("glob");
 var path = require("path");
 var webpack = require("webpack");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var commonConf = {
-  cache: true,
-  watch: true,
-  devtool: 'cheap-module-inline-source-map',
-  context: __dirname,
-  output: {
-    filename: '[name]-bundle.js',
-    path: path.join(__dirname + '/dist'),
-    publicPath: 'http://localhost:3000/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: [ {
-          loader: 'angular-hot-loader',
-          options: {
-            log: false,
-            rootElement: 'html'
-          }
-        }, {
-          loader: 'babel-loader',
-          query: {
-            cacheDirectory: false,
-            presets: ['es2015', 'stage-0'],
-            plugins: ['angular-mark-classes','transform-runtime']
-          }
-        }, 'ts-loader' ],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.pug$/,
-        loader: 'pug-loader'
-      },
-      {
-        test: /\.styl$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'stylus-loader']
-        })
-      },
-
-      { test: /\.svg$/, loader: 'url-loader?limit=65000&mimetype=image/svg+xml&name=public/fonts/[name].[ext]' },
-      { test: /\.woff$/, loader: 'url-loader?limit=65000&mimetype=application/font-woff&name=public/fonts/[name].[ext]' },
-      { test: /\.woff2$/, loader: 'url-loader?limit=65000&mimetype=application/font-woff2&name=public/fonts/[name].[ext]' },
-      { test: /\.[ot]tf$/, loader: 'url-loader?limit=65000&mimetype=application/octet-stream&name=public/fonts/[name].[ext]' },
-      { test: /\.eot$/, loader: 'url-loader?limit=65000&mimetype=application/vnd.ms-fontobject&name=public/fonts/[name].[ext]' }
-    ]
-  },
-  resolve: {
-    modules: ["node_modules"],
-    descriptionFiles: ["package.json"],
-    extensions: [".tsx", ".ts", ".js", ".styl", ".pug", ".css"]
-  },
-  externals: {
-    angular: 'angular',
-    'plotly.js': 'Plotly'
-  },
-  devServer: {
-    hot: true,
-    inline: true,
-    port: 3000,
-    stats: { chunkModules: false }
-  }
-};
+var commonConf = require('./webpack.common.config.js');
+commonConf.output.path: path.join(__dirname + '/dist-dev')
 
 module.exports = [ Object.assign({
   name: 'ui',
---
+++ webpack.deploy.config.js
@@ -0,0 +1,35 @@
+var path = require("path");
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var GhPagesWebpackPlugin = require('gh-pages-webpack-plugin');
+
+var commonConf = require('./webpack.common.config.js');
+commonConf.watch = false
+commonConf.output.path: path.join(__dirname + '/dist')
+
+module.exports = [ Object.assign({
+  name: 'ui',
+  entry: {
+    ui: './app/index.ts'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks: ['ui'],
+      filename: 'index.html',
+      template: 'app/index.pug'
+    }),
+    new ExtractTextPlugin('styles.css'),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new GhPagesWebpackPlugin({
+       path: './dist'
+    })
+  ]
+}, commonConf)
+];
